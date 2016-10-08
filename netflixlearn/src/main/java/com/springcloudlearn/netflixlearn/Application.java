c1b2e85839062d22942acd6ffe529b62aa87ffd9
zhul-a <zhul-a@glodon.com>
2016-10-08 09:39:40 +0800

    update eureka

---
+++ netflixlearn/src/main/java/com/springcloudlearn/netflixlearn/Application.java
@@ -0,0 +1,20 @@
+package com.springcloudlearn.netflixlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.security.AlgorithmConstraints;
+
+/**
+ * Created by zhul-a on 2016/9/5.
+ */
+//开启一个服务注册中心
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
